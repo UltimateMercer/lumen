b5bf289afaa4259ada164f08081578d4ea78f3ff
Julian Silva da Cunha <juliancunha2010@hotmail.com>
2026-07-02 13:24:08 -0300

    ui: papers update padding

--- components/documents/general-components/paper/paper-sheet.tsx
+++ components/documents/general-components/paper/paper-sheet.tsx
@@ -2,7 +2,7 @@
 
 export function PaperSheet({ children }: { children: React.ReactNode }) {
   return (
-    <div className="paper-texture relative mx-auto max-w-3xl border dark:border-[#eaeaea] border-[#252525] p-10 shadow-[0_24px_64px_-32px_oklch(0_0_0/0.6)] md:p-14">
+    <div className="paper-texture relative mx-auto max-w-3xl border dark:border-[#eaeaea] border-[#252525] p-10 shadow-[0_24px_64px_-32px_oklch(0_0_0/0.6)]">
       {children}
     </div>
   );
--- components/documents/general-components/paper/paper.tsx
+++ components/documents/general-components/paper/paper.tsx
@@ -1,6 +1,6 @@
 export const Paper = ({ children }: { children?: React.ReactNode }) => {
   return (
-    <div className="border dark:border-[#eaeaea] border-[#252525] bg-[#eaeaea] dark:bg-[#252525] p-6 max-w-3xl mx-auto">
+    <div className="border dark:border-[#eaeaea] border-[#252525] bg-[#eaeaea] dark:bg-[#252525] p-10 max-w-3xl mx-auto">
       {children}
     </div>
   );
